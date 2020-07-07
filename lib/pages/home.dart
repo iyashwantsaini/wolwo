efaba728b3fcc7673fd48371eff4205529c63808
meyash <yashsn2127@gmail.com>
2020-07-07 14:10:11 +0530

    finalbase

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:random_color/random_color.dart';
 
+import './subpages/wallpaper.dart';
+
 RandomColor _randomColor = RandomColor();
 
 class Home extends StatefulWidget {
