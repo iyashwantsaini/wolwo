46fa550863a8b21d9ad1a51381666bbb790827c0
meyash <yashsn2127@gmail.com>
2020-07-09 00:12:22 +0530

    wallpage

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:device_info/device_info.dart';
+import 'dart:async';
 
 class Settings extends StatefulWidget {
   @override
@@ -6,30 +8,71 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  DeviceInfoPlugin deviceInfo =
+      DeviceInfoPlugin(); // instantiate device info plugin
+  AndroidDeviceInfo androidDeviceInfo;
+  String board,
+      brand,
+      device,
+      hardware,
+      host,
+      id,
+      manufacture,
+      model,
+      product,
+      type,
+      androidid;
+  bool isphysicaldevice;
+  @override
+  void initState() {
+    super.initState();
+    getDeviceinfo();
+  }
+
+  void getDeviceinfo() async {
+    androidDeviceInfo = await deviceInfo
+        .androidInfo; // instantiate Android Device Infoformation
+    setState(() {
+      board = androidDeviceInfo.board;
+      brand = androidDeviceInfo.brand;
+      device = androidDeviceInfo.device;
+      hardware = androidDeviceInfo.hardware;
+      host = androidDeviceInfo.host;
+      id = androidDeviceInfo.id;
+      manufacture = androidDeviceInfo.manufacturer;
+      model = androidDeviceInfo.model;
+      product = androidDeviceInfo.product;
+      type = androidDeviceInfo.type;
+      isphysicaldevice = androidDeviceInfo.isPhysicalDevice;
+      androidid = androidDeviceInfo.androidId;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Color.fromRGBO(206, 147, 216,1),
-      width: double.infinity,
-      // margin: EdgeInsets.all(10),
-      child: Column(
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            height: 40,
-            padding: const EdgeInsets.all(8),
-            child: const Text('Settings!'),
-            color: Colors.teal[200],
-          ),
-          Container(
-            width: double.infinity,
-            height: 40,
-            padding: const EdgeInsets.all(8),
-            child: const Text('Sound of screams but the'),
-            color: Colors.teal[300],
-          ),
-        ],
-      ),
-    );
+        color: Color.fromRGBO(206, 147, 216, 1),
+        width: double.infinity,
+        // margin: EdgeInsets.all(10),
+        child: ListView(
+          padding: const EdgeInsets.all(8),
+          children: <Widget>[
+            Container(
+              height: 50,
+              color: Colors.amber[600],
+              child: const Center(child: Text('Entry A')),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[500],
+              child: const Center(child: Text('Entry B')),
+            ),
+            Container(
+              height: 100,
+              color: Colors.amber[100],
+              child: const Center(child: Text("board")),
+            ),
+          ],
+        ));
   }
 }
