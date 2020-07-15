3981980d83d9a3e3da6ff4afc236633c8389255f
meyash <yashsn2127@gmail.com>
2020-07-15 15:51:56 +0530

    big_changes

--- lib/pages/collections.dart
+++ lib/pages/collections.dart
@@ -16,12 +16,13 @@ class _CollectionsState extends State<Collections> {
   @override
   Widget build(BuildContext context) {
     // used to set size of each image view element
-    // var size = MediaQuery.of(context).size;
+    var size = MediaQuery.of(context).size;
 
     return Container(
       padding: EdgeInsets.fromLTRB(2, 6, 2, 2),
       color: Color.fromRGBO(197, 225, 165, 1),
-      width: double.infinity,
+      width: size.width,
+      height: size.height,
       child: GlowingOverscrollIndicator(
         axisDirection: AxisDirection.down,
         color: Colors.white,
@@ -72,8 +73,8 @@ class _CollectionsState extends State<Collections> {
                               },
                             ))),
                     new Positioned(
-                      right: 10.0,
-                      bottom: 10.0,
+                      top: 40.0,
+                      left: 30.0,
                       child: new Text(
                         'Technology',
                         style: GoogleFonts.hammersmithOne(
@@ -130,126 +131,10 @@ class _CollectionsState extends State<Collections> {
                               },
                             ))),
                     new Positioned(
-                      right: 10.0,
-                      bottom: 10.0,
+                      top: 40.0,
+                      left: 30.0,
                       child: new Text(
                         'Science',
-                        style: GoogleFonts.hammersmithOne(
-                          textStyle: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 25,
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
-                )),
-            Container(
-                // height: 196,
-                width: double.infinity,
-                margin: EdgeInsets.all(3.0),
-                child: new Stack(
-                  children: <Widget>[
-                    new Positioned(
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10.0),
-                        child: Container(
-                          width: double.infinity,
-                          height: 196,
-                          decoration: BoxDecoration(
-                            color: _randomColor.randomColor(
-                                colorBrightness: ColorBrightness.light),
-                          ),
-                          child: Image.network(
-                            'https://wallpaperaccess.com/full/1559285.jpg',
-                            loadingBuilder: (context, child, progress) {
-                              return progress == null
-                                  ? child
-                                  : LinearProgressIndicator();
-                            },
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                    ),
-                    new Positioned.fill(
-                        child: new Material(
-                            color: Colors.transparent,
-                            child: new InkWell(
-                              splashColor: _randomColor.randomColor(
-                                  colorBrightness: ColorBrightness.light),
-                              onTap: () => {
-                                Future.delayed(
-                                    const Duration(milliseconds: 200), () {
-                                  Navigator.push(context,
-                                      ScaleRoute(page: SelectedCategory()));
-                                }),
-                              },
-                            ))),
-                    new Positioned(
-                      right: 10.0,
-                      bottom: 10.0,
-                      child: new Text(
-                        'Art',
-                        style: GoogleFonts.hammersmithOne(
-                          textStyle: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 25,
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
-                )),
-            Container(
-                // height: 196,
-                width: double.infinity,
-                margin: EdgeInsets.all(3.0),
-                child: new Stack(
-                  children: <Widget>[
-                    new Positioned(
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10.0),
-                        child: Container(
-                          width: double.infinity,
-                          height: 196,
-                          decoration: BoxDecoration(
-                            color: _randomColor.randomColor(
-                                colorBrightness: ColorBrightness.light),
-                          ),
-                          child: Image.network(
-                            'https://wallpaperaccess.com/full/1559285.jpg',
-                            loadingBuilder: (context, child, progress) {
-                              return progress == null
-                                  ? child
-                                  : LinearProgressIndicator();
-                            },
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                    ),
-                    new Positioned.fill(
-                        child: new Material(
-                            color: Colors.transparent,
-                            child: new InkWell(
-                              splashColor: _randomColor.randomColor(
-                                  colorBrightness: ColorBrightness.light),
-                              onTap: () => {
-                                Future.delayed(
-                                    const Duration(milliseconds: 200), () {
-                                  Navigator.push(context,
-                                      ScaleRoute(page: SelectedCategory()));
-                                }),
-                              },
-                            ))),
-                    new Positioned(
-                      right: 15.0,
-                      bottom: 10.0,
-                      child: new Text(
-                        'Food',
                         style: GoogleFonts.hammersmithOne(
                           textStyle: TextStyle(
                             color: Colors.white,
--- lib/pages/favourites.dart
+++ lib/pages/favourites.dart
@@ -15,239 +15,168 @@ class _FavouritesState extends State<Favourites> {
   @override
   Widget build(BuildContext context) {
     // used to set size of each image view element
-    final double itemHeight = 344;
+    // final double itemHeight = 344;
     // print(itemHeight);
-    final double itemWidth = 196;
+    // final double itemWidth = 196;
     // print(itemWidth);
+    var size = MediaQuery.of(context).size;
 
     return Container(
-      padding: EdgeInsets.fromLTRB(2, 6, 2, 2),
-      color: Color.fromRGBO(247, 202, 201,1),
-      width: double.infinity,
+      color: Color.fromRGBO(247, 202, 201, 1),
+      // color: Colors.amber,
+      padding: EdgeInsets.fromLTRB(4, 8, 0, 4),
+      width: size.width,
+      height: size.height,
       child: GlowingOverscrollIndicator(
         axisDirection: AxisDirection.down,
         color: Colors.white,
-        child: GridView.count(
-          // physics: const AlwaysScrollableScrollPhysics (),
-          crossAxisCount: 2,
-          childAspectRatio: (itemWidth / itemHeight),
-          controller: new ScrollController(keepScrollOffset: false),
+        child: ListView(
           shrinkWrap: true,
-          scrollDirection: Axis.vertical,
           children: <Widget>[
-            Container(
-              color: Color.fromRGBO(247, 202, 201,1),
-                margin: EdgeInsets.all(3.0),
-                child: new Stack(
-                  children: <Widget>[
-                    new Positioned(
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10.0),
+            Center(
+              child: Row(
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Center(
                         child: Container(
-                          decoration: BoxDecoration(
-                            color: _randomColor.randomColor(
-                                colorBrightness: ColorBrightness.light),
-                          ),
-                          child: Image.network(
-                            'https://wallpaperaccess.com/full/1559285.jpg',
-                            loadingBuilder: (context, child, progress) {
-                              return progress == null
-                                  ? child
-                                  : LinearProgressIndicator();
-                            },
-                            fit: BoxFit.cover,
-                          ),
-                        ),
+                            height: 270,
+                            width: size.width / 2.1,
+                            margin: EdgeInsets.all(3.0),
+                            child: new Stack(
+                              children: <Widget>[
+                                new Positioned(
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    child: Container(
+                                      width: size.width / 2.1,
+                                      height: 270,
+                                      decoration: BoxDecoration(
+                                        color: _randomColor.randomColor(
+                                            colorBrightness:
+                                                ColorBrightness.light),
+                                      ),
+                                      child: Image.network(
+                                        'https://wallpaperaccess.com/full/1559285.jpg',
+                                        loadingBuilder:
+                                            (context, child, progress) {
+                                          return progress == null
+                                              ? child
+                                              : LinearProgressIndicator();
+                                        },
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                new Positioned.fill(
+                                    child: new Material(
+                                        borderRadius:
+                                            BorderRadius.circular(10.0),
+                                        color: Colors.transparent,
+                                        child: new InkWell(
+                                          splashColor: _randomColor.randomColor(
+                                              colorBrightness:
+                                                  ColorBrightness.light),
+                                          onTap: () => {
+                                            Future.delayed(
+                                                const Duration(
+                                                    milliseconds: 200), () {
+                                              Navigator.push(
+                                                  context,
+                                                  ScaleRoute(
+                                                      page: SelectedWall(
+                                                          url:
+                                                              'https://wallpaperaccess.com/full/1559285.jpg')));
+                                            }),
+                                          },
+                                        ))),
+                                new Positioned(
+                                  right: 10.0,
+                                  bottom: 10.0,
+                                  child: new Icon(
+                                    Icons.favorite,
+                                    size: 25,
+                                    color: Colors.pink,
+                                  ),
+                                ),
+                              ],
+                            )),
                       ),
-                    ),
-                    new Positioned.fill(
-                        child: new Material(
-                            color: Colors.transparent,
-                            child: new InkWell(
-                              splashColor: _randomColor.randomColor(
-                                  colorBrightness: ColorBrightness.light),
-                              onTap: () => {
-                                Future.delayed(
-                                    const Duration(milliseconds: 200), () {
-                                  Navigator.push(
-                                      context,
-                                      ScaleRoute(
-                                          page: SelectedWall(
-                                              url:
-                                                  "https://wallpaperaccess.com/full/1559285.jpg")));
-                                }),
-                              },
-                            ))),
-                    new Positioned(
-                      right: 10.0,
-                      bottom: 10.0,
-                      child: new Icon(
-                        Icons.favorite,
-                        size: 25,
-                        color: Colors.pink,
-                      ),
-                    ),
-                  ],
-                )),
-            Container(
-                margin: EdgeInsets.all(3.0),
-                child: new Stack(
-                  children: <Widget>[
-                    new Positioned(
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10.0),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Center(
                         child: Container(
-                          decoration: BoxDecoration(
-                            color: _randomColor.randomColor(
-                                colorBrightness: ColorBrightness.light),
-                          ),
-                          child: Image.network(
-                            'https://wallpaperaccess.com/full/1559285.jpg',
-                            loadingBuilder: (context, child, progress) {
-                              return progress == null
-                                  ? child
-                                  : LinearProgressIndicator();
-                            },
-                            fit: BoxFit.cover,
-                          ),
-                        ),
+                            height: 270,
+                            width: size.width / 2.1,
+                            margin: EdgeInsets.all(3.0),
+                            child: new Stack(
+                              children: <Widget>[
+                                new Positioned(
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    child: Container(
+                                      width: size.width / 2.1,
+                                      height: 270,
+                                      decoration: BoxDecoration(
+                                        color: _randomColor.randomColor(
+                                            colorBrightness:
+                                                ColorBrightness.light),
+                                      ),
+                                      child: Image.network(
+                                        'https://wallpaperaccess.com/full/1559285.jpg',
+                                        loadingBuilder:
+                                            (context, child, progress) {
+                                          return progress == null
+                                              ? child
+                                              : LinearProgressIndicator();
+                                        },
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                new Positioned.fill(
+                                    child: new Material(
+                                        borderRadius:
+                                            BorderRadius.circular(10.0),
+                                        color: Colors.transparent,
+                                        child: new InkWell(
+                                          splashColor: _randomColor.randomColor(
+                                              colorBrightness:
+                                                  ColorBrightness.light),
+                                          onTap: () => {
+                                            Future.delayed(
+                                                const Duration(
+                                                    milliseconds: 200), () {
+                                              Navigator.push(
+                                                  context,
+                                                  ScaleRoute(
+                                                      page: SelectedWall(
+                                                          url:
+                                                              'https://wallpaperaccess.com/full/1559285.jpg')));
+                                            }),
+                                          },
+                                        ))),
+                                new Positioned(
+                                  right: 10.0,
+                                  bottom: 10.0,
+                                  child: new Icon(
+                                    Icons.favorite,
+                                    size: 25,
+                                    color: Colors.pink,
+                                  ),
+                                ),
+                              ],
+                            )),
                       ),
-                    ),
-                    new Positioned.fill(
-                        child: new Material(
-                            color: Colors.transparent,
-                            child: new InkWell(
-                              splashColor: _randomColor.randomColor(
-                                  colorBrightness: ColorBrightness.light),
-                              onTap: () => {
-                                Future.delayed(
-                                    const Duration(milliseconds: 200), () {
-                                  Navigator.push(
-                                      context,
-                                      ScaleRoute(
-                                          page: SelectedWall(
-                                              url:
-                                                  "https://wallpaperaccess.com/full/1559285.jpg")));
-                                }),
-                              },
-                            ))),
-                    new Positioned(
-                      right: 10.0,
-                      bottom: 10.0,
-                      child: new Icon(
-                        Icons.favorite,
-                        size: 25,
-                        color: Colors.pink,
-                      ),
-                    ),
-                  ],
-                )),
-            Container(
-                margin: EdgeInsets.all(3.0),
-                child: new Stack(
-                  children: <Widget>[
-                    new Positioned(
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: _randomColor.randomColor(
-                                colorBrightness: ColorBrightness.light),
-                          ),
-                          child: Image.network(
-                            'https://wallpaperaccess.com/full/1559285.jpg',
-                            loadingBuilder: (context, child, progress) {
-                              return progress == null
-                                  ? child
-                                  : LinearProgressIndicator();
-                            },
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                    ),
-                    new Positioned.fill(
-                        child: new Material(
-                            color: Colors.transparent,
-                            child: new InkWell(
-                              splashColor: _randomColor.randomColor(
-                                  colorBrightness: ColorBrightness.light),
-                              onTap: () => {
-                                Future.delayed(
-                                    const Duration(milliseconds: 200), () {
-                                  Navigator.push(
-                                      context,
-                                      ScaleRoute(
-                                          page: SelectedWall(
-                                              url:
-                                                  "https://wallpaperaccess.com/full/1559285.jpg")));
-                                }),
-                              },
-                            ))),
-                    new Positioned(
-                      right: 10.0,
-                      bottom: 10.0,
-                      child: new Icon(
-                        Icons.favorite,
-                        size: 25,
-                        color: Colors.pink,
-                      ),
-                    ),
-                  ],
-                )),
-            Container(
-                margin: EdgeInsets.all(3.0),
-                child: new Stack(
-                  children: <Widget>[
-                    new Positioned(
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: _randomColor.randomColor(
-                                colorBrightness: ColorBrightness.light),
-                          ),
-                          child: Image.network(
-                            'https://wallpaperaccess.com/full/1559285.jpg',
-                            loadingBuilder: (context, child, progress) {
-                              return progress == null
-                                  ? child
-                                  : LinearProgressIndicator();
-                            },
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                    ),
-                    new Positioned.fill(
-                        child: new Material(
-                            color: Colors.transparent,
-                            child: new InkWell(
-                              splashColor: _randomColor.randomColor(
-                                  colorBrightness: ColorBrightness.light),
-                              onTap: () => {
-                                Future.delayed(
-                                    const Duration(milliseconds: 200), () {
-                                  Navigator.push(
-                                      context,
-                                      ScaleRoute(
-                                          page: SelectedWall(
-                                              url:
-                                                  "https://wallpaperaccess.com/full/1559285.jpg")));
-                                }),
-                              },
-                            ))),
-                    new Positioned(
-                      right: 10.0,
-                      bottom: 10.0,
-                      child: new Icon(
-                        Icons.favorite,
-                        size: 25,
-                        color: Colors.pink,
-                      ),
-                    ),
-                  ],
-                )),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
